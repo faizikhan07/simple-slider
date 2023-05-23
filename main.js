45531eefdc011190abfbb6a7b280b94e3aad6622
Nooshin bakhtiari <nooshindev@gmail.com>
2023-05-23 18:14:06 +0330

    fix the bugs on css and javascript

--- main.js
+++ main.js
@@ -2,40 +2,45 @@
 // select the elements
 const btnRight = document.querySelector('.btn-right');
 const btnLeft = document.querySelector('.btn-left');
-const section = document.querySelectorAll('.section');
+const section = document.querySelectorAll('.section'); // change to an array
+
 
 let currentSlide = 0;
 const maxslide = section.length; // 3
- 
-  section.forEach((s, i) => {
-    console.log(section);
-    s.style.transform = `translateX(${100 * (i - currentSlide)}%)`;
-});
 
-// 0% 100% 200%
-// addEventListener
+// the way that show the first slide for the user
+const showPerson = function () {
+  section.forEach((sections, index) => {
+    sections.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
+    // 100 * i = 0 curr =0 => 100 * 
+    // 100 * i = 1 curr 0 => 100%
+    // 100 * i = 2 curr 0 => 200%
+  });
+};
+showPerson();
+
+//show next person
+
+
+
 btnRight.addEventListener('click', function () {
- 
-  section.forEach((s, i) => {
-    s.style.transform = `translateX(${100 * (i - currentSlide)}%)`;
-});
-
-  if (currentSlide === maxslide -1) {
+  if (currentSlide === maxslide - 1) {
     currentSlide = 0;
   } else {
     currentSlide++;
   }
+ 
+  showPerson();
+ 
 });
 
+//show last person
 btnLeft.addEventListener('click', function () {
-   
-   if (currentSlide === 0) {
+  if (currentSlide === 0) {
     currentSlide = maxslide - 1;
   } else {
-    currentSlide --;
+    currentSlide--;
   }
 
-  section.forEach((s, i) => {
-    s.style.transform = `translateX(${100 * (i - currentSlide)}%)`;
-});
+  showPerson();
 });
