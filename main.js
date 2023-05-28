0c77c1c67bbcc8698508d7223d9bd09afbacc3dd
Nooshin bakhtiari <nooshindev@gmail.com>
2023-05-28 12:08:56 +0330

    add logic to javascript file

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const btnRight = document.querySelector('.btn-right');
 const btnLeft = document.querySelector('.btn-left');
 const section = document.querySelectorAll('.section'); // change to an array
 
-
 let currentSlide = 0;
 const maxslide = section.length; // 3
 
@@ -12,7 +11,7 @@ const maxslide = section.length; // 3
 const showPerson = function () {
   section.forEach((sections, index) => {
     sections.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
-    // 100 * i = 0 curr =0 => 100 * 
+    // 100 * i = 0 curr =0 => 100 *
     // 100 * i = 1 curr 0 => 100%
     // 100 * i = 2 curr 0 => 200%
   });
@@ -21,21 +20,16 @@ showPerson();
 
 //show next person
 
-
-
-btnRight.addEventListener('click', function () {
+const nextSlide = function () {
   if (currentSlide === maxslide - 1) {
     currentSlide = 0;
   } else {
     currentSlide++;
   }
- 
   showPerson();
- 
-});
+};
 
-//show last person
-btnLeft.addEventListener('click', function () {
+const prerviewSlide = function () {
   if (currentSlide === 0) {
     currentSlide = maxslide - 1;
   } else {
@@ -43,4 +37,15 @@ btnLeft.addEventListener('click', function () {
   }
 
   showPerson();
+};
+btnRight.addEventListener('click', nextSlide);
+//show last person
+btnLeft.addEventListener('click', prerviewSlide);
+
+// document
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+
+  if (e.key === 'ArrowRight') nextSlide();
+  if (e.key === 'ArrowLeft') prerviewSlide();
 });
